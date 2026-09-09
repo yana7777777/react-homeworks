ed3c54e3878a8ec4f46b2658771c5ec39a29d383
Яна Чувакова <yanachuvakova7770@gmail.com>
2026-09-09 14:10:08 +0300

    Homework 5: events handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 import './App.css';
 
+let clickCount = 0;
+
 function App() {
-    const name = 'Яна';
+    function handleClick() {
+        clickCount = clickCount + 1;
+        console.log('Нажатий:', clickCount);
+    }
+
+    function handleChange(event) {
+        console.log('Имя:', event.target.value);
+    }
+
+    function handleMouse() {
+        console.log('Мышь наведена!');
+    }
 
     return (
         <div className="card">
-            <h1>Привет, меня зовут {name}</h1>
-            <h2>Моя профессия: начинающий разработчик</h2>
-            <p>Я учусь создавать интерфейсы на React. </p>
-            <p>"Можно завоевать весь мир и тогда любое место станет твоим домом."</p>
+            <h1>Домашнее задание 5</h1>
+
+            <button onClick={handleClick}>Нажми меня</button>
+            <p>Количество нажатий смотрите в консоли</p>
+
+            <hr />
+
+            <input
+                type="text"
+                placeholder="Введите имя"
+                onChange={handleChange}
+            />
+            <p>Смотрите имя в консоли</p>
+
+            <hr />
+
+            <div className="highlight" onMouseOver={handleMouse}>
+                Наведи на меня
+            </div>
         </div>
     );
 }
